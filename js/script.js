cd1d59a06a235ca14d8bab05c84ae019d133d147
unknown <guilhermefloyd2017@gmail.com>
2023-03-19 00:37:49 +0044

    Portifolio

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+//funçao para os arquivos jquery
+
+$(function(){
+
+    abrirpastas()
+    function abrirpastas(){
+        $('.header').click(function(){
+            var fb =$(this).attr('href')
+            $.ajax();{
+                'url'; href
+               
+            } return false
+           
+        })
+
+     
+        
+    }
+})
